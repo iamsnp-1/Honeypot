ca3fd4d822fbaf31b50eaf9a5a2a51cb295c05ee
iamsnp-1 <suyashpatil878@gmail.com>
2026-02-05 19:44:08 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from .router import router
+from fastapi import FastAPI, Header, HTTPException
+
+app = FastAPI()
+
+API_KEY = "CHANGE_THIS_SECRET_KEY"
 
 app = FastAPI(title="Agentic HoneyPot API")
 app.include_router(router)
+
