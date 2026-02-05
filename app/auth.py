9f5db58360edba8618e3a5e0cbe8eddf3f31fecd
iamsnp-1 <suyashpatil878@gmail.com>
2026-02-05 23:09:35 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,13 @@
 from fastapi import Header, HTTPException
-from app.config import API_KEY
+import os
 
-def verify_api_key(x_api_key: str = Header(...)):
+API_KEY = os.getenv("API_KEY", "CHANGE_THIS_SECRET_KEY")
+
+def verify_api_key(
+    x_api_key: str = Header(..., alias="x-api-key")
+):
     if x_api_key != API_KEY:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid API key"
+        )
