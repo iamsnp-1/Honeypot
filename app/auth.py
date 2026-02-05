d7f7584718bded0adcb34e774f8d8301dfb9fb85
iamsnp-1 <suyashpatil878@gmail.com>
2026-02-06 03:22:50 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -2,8 +2,9 @@ from fastapi import Header, HTTPException
 from app.config import API_KEY
 import os
 
-API_KEY = os.getenv("API_KEY", "CHANGE_THIS_SECRET_KEY")
+API_KEY = os.getenv("API_KEY", "guvi-honeypot-2026-secure")
 def verify_api_key(x_api_key: str = Header(...)):
     if x_api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Unauthorized")
+
 
