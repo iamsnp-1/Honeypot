b5c86bda8cb55ddf0980acbbc67265d100267f12
iamsnp-1 <suyashpatil878@gmail.com>
2026-02-06 00:27:42 +0530

    Delete Agent directory

--- Agent/mock_llm.py
+++
@@ -1,7 +0,0 @@
-class MockLLM:
-    def generate(self, system, prompt, max_tokens=60):
-        """
-        Deterministic mock response.
-        This simulates a confused but cooperative human reply.
-        """
-        return "I’m not sure… can you explain once?"
